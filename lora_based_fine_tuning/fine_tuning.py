24eb0b6feb87869d865c44fe2955dc0895d8a6e0
Sangh0 <hop7311@naver.com>
2024-10-13 00:47:43 +0900

    chore: add optional parameter for each functions

--- lora_based_fine_tuning/fine_tuning.py
+++ lora_based_fine_tuning/fine_tuning.py
@@ -1,15 +1,25 @@
-import torch
-
 from dataclasses import dataclass
-from typing import Tuple, Dict
-
-from datasets import load_dataset
-from transformers import AutoModelForCausalLM, AutoTokenizer, TrainingArguments
+from typing import Tuple, Dict, Optional, Union
+from datasets import (
+    load_dataset,
+    Dataset,
+    DatasetDict,
+    IterableDataset,
+    IterableDatasetDict,
+)
+from transformers import (
+    AutoModelForCausalLM,
+    AutoTokenizer,
+    TrainingArguments,
+    BitsAndBytesConfig,
+)
 from peft import LoraConfig
 from trl import SFTTrainer
 
 
-def get_dataset(dataset_name: str, split: str = "train") -> torch.utils.data.Dataset:
+def get_dataset(
+    dataset_name: str, split: str = "train"
+) -> Union[DatasetDict, Dataset, IterableDatasetDict, IterableDataset]:
     """Load a dataset from the Hugging Face datasets library.
     Args:
         dataset_name (`str`): The name of the dataset to load.
@@ -21,16 +31,25 @@ def get_dataset(dataset_name: str, split: str = "train") -> torch.utils.data.Dat
 
 def get_model_and_tokenizer(
     model_name: str,
+    bnb_config: Optional[BitsAndBytesConfig] = None,
     use_cache: bool = False,
     pretraining_tp: int = 1,
+    device_map: Dict = {"": 0},
 ) -> Tuple[AutoModelForCausalLM, AutoTokenizer]:
     """Load a model and tokenizer from the Hugging Face model hub.
     Args:
         model_name (`str`): The name of the model to load.
+        bnb_config (`Optional[BitsAndBytesConfig]`): The Bits and Bytes configuration for quantization.
         use_cache (`bool`): Whether to use the cache for the model.
         pretraining_tp (`int`): The number of tensor parallelism, if set to 1, active only one GPU device.
+        device_map (`Dict`): The device map for tensor parallelism.
     """
-    model = AutoModelForCausalLM.from_pretrained(model_name)
+
+    model = AutoModelForCausalLM.from_pretrained(
+        model_name,
+        quantization_config=bnb_config,
+        device_map=device_map,
+    )
     model.config.use_cache = use_cache
     model.config.pretraining_tp = pretraining_tp
 
@@ -68,9 +87,9 @@ class TrainingArgs:
     output_dir: str = "./output"
     eval_strategy: str = "epoch"
     per_device_train_batch_size: int = 4
-    per_device_eval_batch_size: int = 2
+    per_device_eval_batch_size: Optional[int] = None
     gradient_accumulation_steps: int = 1
-    learning_rate: float = 5e-5
+    lr: float = 5e-5
     weight_decay: float = 0.0
     max_grad_norm: float = 3.0
     epochs: int = 10
@@ -79,8 +98,8 @@ class TrainingArgs:
     logging_strategy: str = "epoch"
     save_total_limit: int = 1
     bf16: bool = False
-    fp16: bool = False
-    load_best_model_at_end: bool = True
+    fp16: bool = True
+    load_best_model_at_end: Optional[bool] = None
     optim: str = "adamw_torch"
     report_to: str = "tensorboard"
 
@@ -116,12 +135,16 @@ def get_training_arguments(args: TrainingArgs) -> TrainingArguments:
 def model_training(
     model: AutoModelForCausalLM,
     tokenizer: AutoTokenizer,
-    train_dataset: torch.utils.data.Dataset,
-    eval_dataset: torch.utils.data.Dataset,
+    train_dataset: Union[DatasetDict, Dataset, IterableDatasetDict, IterableDataset],
+    eval_dataset: Optional[
+        Union[DatasetDict, Dataset, IterableDatasetDict, IterableDataset]
+    ],
     peft_config: LoraConfig,
     training_arguments: TrainingArguments,
+    dataset_text_field: str = "text",
+    max_seq_length: int = 512,
 ) -> Dict:
-    """Main function to train a model using LoRA-based fine-tuning.
+    """Key function to train a model using LoRA-based fine-tuning.
     Args:
         model (`AutoModelForCausalLM`): The model to train.
         tokenizer (`AutoTokenizer`): The tokenizer to use.
@@ -129,6 +152,8 @@ def model_training(
         eval_dataset (`torch.utils.data.Dataset`): The evaluation dataset.
         peft_config (`LoraConfig`): The LoRA configuration to use.
         training_arguments (`TrainingArguments`): The training arguments to use.
+        dataset_text_field (`str`): The text field column in the dataset.
+        max_seq_length (`int`): The maximum sequence length for the input of model.
     """
     trainer = SFTTrainer(
         model=model,
@@ -137,7 +162,8 @@ def model_training(
         eval_dataset=eval_dataset,
         peft_config=peft_config,
         args=training_arguments,
-        dataset_text_field="text",
+        dataset_text_field=dataset_text_field,
+        max_seq_length=max_seq_length,
     )
 
     trainer.train()
