8debc7195f06ddaaf1a3cc6f2a17e25dd92b2193
Sangh0 <hop7311@naver.com>
2024-10-14 23:31:49 +0900

    chore: moved utilization and models code

--- lora_based_fine_tuning/fine_tuning.py
+++ lora_based_fine_tuning/fine_tuning.py
@@ -1,7 +1,6 @@
 from dataclasses import dataclass
-from typing import Tuple, Dict, Optional, Union
+from typing import Optional, Union, Dict
 from datasets import (
-    load_dataset,
     Dataset,
     DatasetDict,
     IterableDataset,
@@ -11,53 +10,9 @@ from transformers import (
     AutoModelForCausalLM,
     AutoTokenizer,
     TrainingArguments,
-    BitsAndBytesConfig,
 )
 from peft import LoraConfig
 from trl import SFTTrainer
-
-
-def get_dataset(
-    dataset_name: str, split: str = "train"
-) -> Union[DatasetDict, Dataset, IterableDatasetDict, IterableDataset]:
-    """Load a dataset from the Hugging Face datasets library.
-    Args:
-        dataset_name (`str`): The name of the dataset to load.
-        split (`str`): The split of the dataset to load. Must be one of "train" or "validation".
-    """
-    assert split in ("train", "validation")
-    return load_dataset(dataset_name, split=split)
-
-
-def get_model_and_tokenizer(
-    model_name: str,
-    bnb_config: Optional[BitsAndBytesConfig] = None,
-    use_cache: bool = False,
-    pretraining_tp: int = 1,
-    device_map: Dict = {"": 0},
-) -> Tuple[AutoModelForCausalLM, AutoTokenizer]:
-    """Load a model and tokenizer from the Hugging Face model hub.
-    Args:
-        model_name (`str`): The name of the model to load.
-        bnb_config (`Optional[BitsAndBytesConfig]`): The Bits and Bytes configuration for quantization.
-        use_cache (`bool`): Whether to use the cache for the model.
-        pretraining_tp (`int`): The number of tensor parallelism, if set to 1, active only one GPU device.
-        device_map (`Dict`): The device map for tensor parallelism.
-    """
-
-    model = AutoModelForCausalLM.from_pretrained(
-        model_name,
-        quantization_config=bnb_config,
-        device_map=device_map,
-    )
-    model.config.use_cache = use_cache
-    model.config.pretraining_tp = pretraining_tp
-
-    tokenizer = AutoTokenizer.from_pretrained(model_name)
-    tokenizer.pad_token = tokenizer.eos_token
-    tokenizer.padding_side = "right"
-
-    return model, tokenizer
 
 
 @dataclass
