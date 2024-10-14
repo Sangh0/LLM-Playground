20bbcf47f4f775a26583c51676ee47a7759ef146
Sangh0 <hop7311@naver.com>
2024-10-14 23:34:40 +0900

    chore: add annotations in functions

--- lora_based_fine_tuning/unsloth_models.py
+++ lora_based_fine_tuning/unsloth_models.py
@@ -12,6 +12,15 @@ def get_unsloth_model_and_tokenizer(
     device_map: Dict = {"": 0},
     trust_remote_mode: bool = False,
 ):
+    """Use the unsloth framework to load a model and tokenizer from the Hugging Face model hub.
+    Args:
+        model_name (`str`): The name of the model to load.
+        max_seq_length (`int`): The maximum sequence length for the model.
+        dtype (`Optional[Union[str, torch.dtype]]`): The data type for the model.
+        load_in_4bit (`bool`): Whether to load the model in 4-bit mode.
+        device_map (`Dict`): The device map for tensor parallelism.
+        trust_remote_mode (`bool`): Whether to trust the remote mode for the model.
+    """
     model, tokenizer = FastLanguageModel.from_pretrained(
         model_name=model_name,
         max_seq_length=max_seq_length,
@@ -32,6 +41,15 @@ def get_unsloth_lora_model(
     bias: str = "none",
     target_modules: Union[List[str], str] = ["q_proj", "v_proj"],
 ):
+    """
+    Args:
+        model: The model to load used unsloth.
+        lora_r: The number of tensor parallelism.
+        lora_alpha: The number of bits for quantization.
+        lora_dropout: The dropout rate for LoRA.
+        bias: The bias for LoRA.
+        target_modules: The target modules for Lo
+    """
     model = FastLanguageModel.get_peft_model(
         model,
         r=lora_r,
